29291a9e655650a88c4b37b18b338190d7499882
aczietlow <aczietlow@gmail.com>
2025-09-30 18:41:54 -0400

    added method to parse urls from html

--- parse.go
+++ parse.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -26,4 +29,33 @@ func getFirstParagraphFromHTML(html string) (string, error) {
 	firstPara := doc.Find("p").First().Text()
 
 	return firstPara, nil
+}
+
+func getURLsFromHtml(htmlBody string, baseURL *url.URL) ([]string, error) {
+	urls := make([]string, 0)
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
+	if err != nil {
+		return []string{}, err
+	}
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		if href, exists := s.Attr("href"); exists {
+			fmt.Println("found an anchor")
+			// TODO: normalize rel and abs urls
+			u, err := url.Parse(href)
+			if err != nil {
+				// TODO: figure out how to error here
+				log.Println("Failed to parse url")
+			}
+
+			if u.IsAbs() {
+				urls = append(urls, href)
+			} else {
+				baseURL.Path = href
+				urls = append(urls, baseURL.String())
+			}
+
+		}
+	})
+
+	return urls, nil
 }
